c1dbf87bbf0206bf2beb0f87c7e5e25cf3750740
Washington Charles <toncharles@live.com>
2021-10-13 20:31:09 -0300

    New Room Page finished

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,36 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Illustracao" />
+        <strong>Toda pergunta tem uma resposta</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Crie uma nova sala</h2>
+          <form>
+            <input 
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>
+          Quer entrar em uma sala já existente? <a href="#">Clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
