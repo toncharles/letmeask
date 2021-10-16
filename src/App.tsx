f31ef1d9757cbb5f757bbfda9fd75d40456318e8
Washington Charles <toncharles@live.com>
2021-10-15 22:32:35 -0300

    route correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {AuthContextProvider} from './contexts/AuthContext';
 
 import { Home } from "./pages/Home";
@@ -9,9 +9,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" exact component={NewRoom} />
-        <Route path="/rooms/:id" component={Room} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
         </AuthContextProvider>
     </BrowserRouter>
   );
