33c8bad540e08fb4eba91a731b9486641a85a2cf
Washington Charles <toncharles@live.com>
2021-10-15 22:40:38 -0300

    adjustment in the passage of the room creation parameter

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import {FormEvent, useState} from 'react';
 import { Button } from '../components/Button';
 import { database } from '../services/firebase';
@@ -10,6 +10,7 @@ import '../styles/auth.scss';
 
 export function NewRoom() {
   const {user} = useAuth();
+  const history = useHistory();
   const [newRoom, setNewRoom] = useState('');
 
   async function handleCreateRoom(event:FormEvent) {
@@ -25,6 +26,8 @@ export function NewRoom() {
       title: newRoom,
       authorId: user?.id
     })
+
+    history.push(`/rooms/${firebaseRoom.key}`);
   }
 
   return (
