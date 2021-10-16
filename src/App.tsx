e659b8799809def761fb569e3ff7d6c02c945a9e
Washington Charles <toncharles@live.com>
2021-10-15 22:28:58 -0300

    room page routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import {AuthContextProvider} from './contexts/AuthContext';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/rooms/new" exact component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
         </AuthContextProvider>
     </BrowserRouter>
   );
