96f335aafa3a358a80c6a7d8372f34562453ed37
Washington Charles <toncharles@live.com>
2021-10-17 15:16:33 -0300

    RoomPage create

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,41 @@
+import {useParams} from 'react-router-dom';
+import '../styles/room.scss';
+import { Button } from '../components/Button';
+import logoImg from '../assets/images/logo.svg';
+import { RoomCode } from '../components/RoomCode';
+
+type RoomParams = {
+  id: string;
+}
+
 export function Room() {
+  const params = useParams<RoomParams>();
+
   return (
-    <h1>Sala</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <RoomCode code={params.id} />
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>5 perguntas</span>
+        </div>
+
+        <form>
+          <textarea 
+            placeholder= "o que voce quer perguntar?" 
+          />
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faca seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
