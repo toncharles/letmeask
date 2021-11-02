a4764a9543785bede13ae7872eb61e5b1d9ad1d5
Washington Charles <toncharles@live.com>
2021-11-02 17:11:11 -0300

    question removal

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -5,8 +5,10 @@ import { RoomCode } from '../components/RoomCode';
 import '../styles/room.scss';
 import { Button } from '../components/Button';
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
 
 type RoomParams = {
   id: string;
@@ -18,6 +20,12 @@ export function AdminRoom() {
   const roomId = params.id;
 
   const {title, questions} = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string){
+    if (window.confirm('Tem certeza que deseja excluir essa pergunta?')){
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
   return (
     <div id="page-room">
@@ -45,7 +53,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+              <button
+                type="button"
+                onClick={() => handleDeleteQuestion(question.id)}
+              >
+                <img src={deleteImg} alt="Remover a Pergunta" />
+              </button>
+              </Question>
             );
           })}
         </div>
